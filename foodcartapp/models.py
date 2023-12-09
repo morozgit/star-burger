3fd7acfe549f44a11970c94f5540d5ded1a726ab
morozgit <morozec76@gmail.com>
2023-12-09 20:40:18 +0300

    delete default

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -243,9 +243,7 @@ class OrderItem(models.Model):
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=0
     )
     quantity = models.IntegerField(
         verbose_name='количество',
-        default=0
     )
