412e1137d1693d88668472cdc5c5656421981842
morozgit <morozec76@gmail.com>
2024-02-06 20:10:41 +0300

    Update models.py

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -241,7 +241,6 @@ class OrderItem(models.Model):
     )
     price = models.DecimalField(
         max_digits=8,
-        max_digits=8,
         decimal_places=2,
         validators=[MinValueValidator(0)],
         validators=[MinValueValidator(0)]
